6ca0c1b07e1df74abcfa821e28397dd6c15ba024
Mateusz Koziel <mateyuszafgag@gmail.com>
2023-05-17 10:14:32 +0200

    pull data from database instead of file

--- flaskr/addquote.py
+++ flaskr/addquote.py
@@ -1,8 +1,5 @@
 from flask import Blueprint, request
-import json, os
-
-with open(os.path.abspath(os.path.join(os.path.dirname(__file__), 'quotes.json')), "r", encoding='utf-8') as quotesFileForRead:
-  quotesObject = json.load(quotesFileForRead)
+from init_db import get_db_connection
 
 status_codes = {500: ("<b>500 Internal server error<b>", 500), 400: ("<b>400 Bad request</b>", 400), 200: ("<b>Ok</b>", 200)}
 
@@ -10,24 +7,22 @@ status_codes = {500: ("<b>500 Internal server error<b>", 500), 400: ("<b>400 Bad
 addquote_blueprint = Blueprint("addquote", __name__)
 @addquote_blueprint.route('/addquote', methods=['POST'])
 def addquote():
+    connection = get_db_connection()
+    cursor = connection.cursor()
     userInputJson = request.get_json()
 
     # checking if user input is correct
-    try:
-        if(not len(userInputJson["author"]) or not len(userInputJson["quote"])):
-            raise Exception
-    except Exception:
+    if(not len(userInputJson["author"]) or not len(userInputJson["quote"])):
         return status_codes[400]
 
-    # collecting user input for later adding into file
+    # collecting user input for later adding into database
     newquote = {}
     newquote["quote"] = userInputJson["quote"]
     newquote["author"] = userInputJson["author"]
-    quotesObject["quotes"].append(newquote)
-    # trying to add the new quote to the quotes file
+    # trying to add the new quote to the database
     try:
-        quotesFileForWrite = open('quotes.json', "w", encoding='utf-8')
-        json.dump(quotesObject, quotesFileForWrite)
+        cursor.execute(f"INSERT INTO quotes (author, quote) VALUES ('{newquote['author']}', '{newquote['quote']}')")
+        connection.commit()
         return status_codes[200]
     except Exception as error:
         return f"{error} ", status_codes[500]
